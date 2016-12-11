96fd06de272886288bf1d619237df5126e1a743c
1337sh33p <ma6inoo@gmail.com>
2016-12-11 22:25:52 +0200

    Add more tasks

---
+++ CSharp Fundamentals/02. Data types and variables/ASCIIprint/printASCII.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ASCIIprint
+{
+    class printASCII
+    {
+        static void Main()
+        {
+            for (byte number = 33; number <= 126; number++)
+                {
+                Console.Write((char)number);
+                }
+        }
+    }
+}
