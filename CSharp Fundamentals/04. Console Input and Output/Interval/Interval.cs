33b3d9c2ea604c2821415adb6e37e05947e5f620
1337sh33p <ma6inoo@gmail.com>
2017-02-26 14:55:00 +0200

    Adeed homework assignments

---
+++ CSharp Fundamentals/04. Console Input and Output/Interval/Interval.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interval
+{
+    class Interval
+    {
+        static void Main()
+        {
+            int min = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = min + 1; i < max; i++)
+            {
+                if (i % 5 == 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
