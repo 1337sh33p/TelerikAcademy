d12a7134c698b32fc8b3751d59511dcbc3510cdb
1337sh33p <ma6inoo@gmail.com>
2017-02-26 14:33:24 +0200

    Added homework tasks for third lecture

---
+++ CSharp Fundamentals/03. Operators and Expressions/ThirdDigit/ThirdDigit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ThirdDigit
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = (a / 100);
+        int c = (b % 10);
+        if (c == 7)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false " + c);
+        }
+    }
+}
---
+++ CSharp Fundamentals/03. Operators and Expressions/nThBit/nThBit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class nThBit
+{
+    static void Main(string[] args)
+    {
+        long number = long.Parse(Console.ReadLine());
+        int possition = int.Parse(Console.ReadLine());
+        long mask = 1 << possition;
+        long numAndMask = number & mask;
+        long thirdBit = numAndMask >> possition;
+
+        Console.WriteLine(thirdBit);
+    }
+}
