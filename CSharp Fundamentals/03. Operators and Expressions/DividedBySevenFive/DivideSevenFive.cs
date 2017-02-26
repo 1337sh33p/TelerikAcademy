2ca8a230a50834950e9ca263b69199467dfd21d0
1337sh33p <ma6inoo@gmail.com>
2017-02-26 14:54:38 +0200

    Added homework assignments

---
+++ CSharp Fundamentals/03. Operators and Expressions/DividedBySevenFive/DivideSevenFive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class DivideSevenFive
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        if (x % 35 == 0)
+        {
+            Console.WriteLine("true " + x);
+        }
+        else
+        {
+            Console.WriteLine("false " + x);
+        }
+    }
+}
