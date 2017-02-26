48862c0832ba3d0b3339de09136dedaba3206b39
1337sh33p <ma6inoo@gmail.com>
2017-02-26 14:55:22 +0200

    Added homework assignments

---
+++ CSharp Fundamentals/05. Conditional Statements/ExchangeIfGreater/ExchangeIfGreater.cs
@@ -0,0 +1,56 @@
+﻿/*Description
+
+Write a program that reads two double values from the console A and B, stores them in variables and exchanges their values if the first one is greater than the second one. 
+Use an if-statement. As a result print the values of the variables A and B, separated by a space.
+Input
+
+    On the first line, you will receive the value of A
+    On the second line, you will receive the value of B
+
+Output
+
+    On the only output line, print the values of the two variables, separated by a whitespace
+
+Constraints
+
+    A and B will always be valid real numbers between -100 and 100
+    Time limit: 0.1s
+    Memory limit: 16MB
+
+Sample tests
+Input 	Output
+5
+3 	3 5
+2
+4 	2 4
+3.3
+3.3 	3.3 3.3*/
+using System;
+
+namespace ExchangeIfGreater
+{
+    class ExchangeIfGreater
+    {
+        static void Main()
+        {
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+
+            if ((A < -100 || A > 100) || (B < -100 || B > 100))
+            {
+                Console.WriteLine("Number out of range!");
+            }
+            else if (A > B)
+            {
+                double temp = A;
+                A = B;
+                B = temp;
+                Console.WriteLine("{0} {1}", A, B);
+            }
+            else if (B > A)
+            {
+                Console.WriteLine("{0} {1}", A, B);
+            }    
+        }
+    }
+}
---
+++ CSharp Fundamentals/05. Conditional Statements/IntDoubleString/IntDoubleString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntDoubleString
+{
+    class IntDoubleString
+    {
+        static void Main()
+        {
+            string choice = Console.ReadLine();
+            string value = Console.ReadLine();
+            int whole;
+            double real;
+            Int32.TryParse(value, out whole);
+            Double.TryParse(value, out real);
+
+            switch (choice)
+            {
+                case "integer": Console.WriteLine("{0}", whole + 1); break;
+                case "real": Console.WriteLine("{0:F2}", real + 1); break;
+                case "text": Console.WriteLine("{0}", value + "*"); break;
+            }
+        }
+    }
+}
