cfb2b82a1fba331d8c93024a6dc2509fba02bbe0
1337sh33p <ma6inoo@gmail.com>
2017-03-06 20:02:01 +0200

    Added an exercise

---
+++ 01. CSharp Fundamentals/04. Console Input and Output/NumberFromOneToN/NumberFromOneToN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumberFromOneToN
+{
+    class NumberFromOneToN
+    {
+        static void Main()
+        {
+            uint number = uint.Parse(Console.ReadLine());
+            uint counter = 1;
+
+            while (counter <= number)
+            {
+                Console.Write("{0} ",counter);
+                counter++;
+            }
+        }
+    }
+}
