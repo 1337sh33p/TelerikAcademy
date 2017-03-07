ebda6f647643460c92b216c9cce614c49aea83c3
1337sh33p <ma6inoo@gmail.com>
2017-03-07 09:53:16 +0200

    Added new tasks

---
+++ 01. CSharp Fundamentals/06. Loops/CalculateThree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+namespace CalculateThree
+{
+    class Program
+    {
+        public static BigInteger FactorialCalculator(int number)
+        {
+            BigInteger result = 1;
+            int factorial = 1;
+
+            while (factorial <= number)
+            {
+                result *= factorial;
+                factorial++;
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            BigInteger result = FactorialCalculator(n) / (FactorialCalculator(k) * (FactorialCalculator(n - k)));
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01. CSharp Fundamentals/06. Loops/Odd-and-Even-Product/Odd-and-Even.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Odd_and_Even_Product
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string inputNumbers = Console.ReadLine();
+            string[] splitNumbers = inputNumbers.Split(' ');
+
+            int[] numbers = new int[n];
+            long odd = 1;
+            long even = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(splitNumbers[i]);
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(i % 2 ==0)
+                {
+                    odd *= numbers[i];
+                }
+                else
+                {
+                    even *= numbers[i];
+                }
+            }
+            if (odd == even)
+            {
+                Console.WriteLine("yes {0}", odd);
+            }
+            else
+            {
+                Console.WriteLine("no {0} {1}", odd, even);
+            }
+  
+        }
+    }
+}
