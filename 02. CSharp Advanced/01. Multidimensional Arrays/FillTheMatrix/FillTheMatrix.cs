ac3130a0b4e02d681d3e4a4f1811f086b3722534
1337sh33p <ma6inoo@gmail.com>
2017-03-08 10:21:55 +0200

    Added tasks

---
+++ 02. CSharp Advanced/01. Multidimensional Arrays/FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sizeN = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[sizeN,sizeN];
+        int count = 1;
+        int counter = 0;
+        int counting = 1;
+        int caseB = sizeN - 1;
+        char variant = char.Parse(Console.ReadLine());
+
+        for (int row = 0; row < sizeN; row++)
+            {
+            for (int col = 0; col < sizeN; col++)
+                {
+                matrix[row, col] = counter + 1;
+                counter++;
+                }
+            }
+
+        switch (variant)
+        {
+            case 'a':
+                for (int col = 0; col < sizeN; col++)
+                {
+                    for (int row = 0; row < sizeN; row++)
+                    {
+                        Console.Write("{0}", matrix[row, col]);
+                        if (row == sizeN - 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                   
+                }
+                break;
+
+            case 'b':
+                for (int row = 0; row < sizeN; row++)
+                {
+                    for (int col = 0; col < sizeN; col++)
+                    {
+                        if (col == 0 || col % 2 == 0)
+                        {
+                            Console.Write("{0}", matrix[col, row]);
+                            if (col != sizeN - 1)
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("{0}", matrix[col, sizeN- row - 1]);
+                            if (col != sizeN -1)
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                break;
+
+            case 'c':
+                int rows = sizeN - 1;
+                int cols = 0;
+                for (int i = 0; i < sizeN * sizeN; i++)
+                {
+                    matrix[rows, cols] = count;
+                    rows++;
+                    cols++;
+                    count++;
+
+                    if (rows == sizeN || cols == sizeN)
+                    {
+                        rows--;
+                        if (cols == sizeN)
+                        {
+                            rows--;
+                            cols--;
+                        }
+                        while (rows - 1 >= 0 && cols - 1 >= 0)
+                        {
+                            rows--;
+                            cols--;
+                        }
+                    }
+                }
+                for (int col = 0; col < sizeN; col++)
+                {
+                    for (int row = 0; row < sizeN; row++)
+                    {
+                        Console.Write("{0}",matrix[col, row] );
+                        if (row != sizeN - 1)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                break;
+
+            case 'd':
+
+                int rowS = 0;
+                int colS = 0;
+                int maxRows = sizeN - 1;
+                int maxCols = sizeN - 1;
+
+                do
+                {
+                    for (int i = rowS; i <= maxRows; i++)
+                    {
+                        matrix[i, colS] = counting;
+                        counting++;
+                    }
+                    colS++;
+
+                    for (int i = colS; i <= maxCols; i++)
+                    {
+                        matrix[maxRows, i] = counting;
+                        counting++;
+                    }
+                    maxRows--;
+
+                    for (int i = maxRows; i >= rowS; i--)
+                    {
+                        matrix[i, maxCols] = counting;
+                        counting++;
+                    }
+                    maxCols--;
+
+                    for (int i = maxCols; i >= colS; i--)
+                    {
+                        matrix[rowS, i] = counting;
+                        counting++;
+                    }
+                    rowS++;
+                }
+                while (counting <= sizeN * sizeN);
+
+                for (int col = 0; col < sizeN; col++)
+                {
+                    for (int row = 0; row < sizeN; row++)
+                    {
+                        Console.Write("{0}", matrix[col, row]);
+                        if (row != sizeN - 1)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                break;
+        }
+    }
+}
---
+++ 02. CSharp Advanced/01. Multidimensional Arrays/MaximalSum/MaximalSum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string maxSize = Console.ReadLine();
+        string [] arr = maxSize.Split(' ');
+        int N, M;
+
+        bool first = Int32.TryParse(arr[0], out N);
+        bool second = Int32.TryParse(arr[1], out M);
+
+        int[,] theMatrix = new int[N, M];
+        int maxSum = int.MinValue;
+
+        for (int rows = 0; rows < N; rows++)
+        {
+            string numbers = Console.ReadLine();
+            string[] theSplit = numbers.Split(' ');
+
+            for (int col = 0; col < M; col++)
+            {
+                bool truth = Int32.TryParse(theSplit[col], out theMatrix[rows, col]);   
+            }
+        }
+
+        for (int row = 0; row < theMatrix.GetLength(0) - 2; row++)
+        {
+            for (int col = 0; col < theMatrix.GetLength(1) - 2; col++)
+            {
+                int sum = theMatrix[row, col] + theMatrix[row + 1, col] + theMatrix[row + 2, col] + theMatrix[row, col + 1] + theMatrix[row, col + 2] + theMatrix[row + 1, col + 1] + theMatrix[row + 1, col + 2] + theMatrix[row + 2, col + 1] + theMatrix[row + 2, col + 2];
+
+                if (maxSum < sum)
+                {
+                    maxSum = sum;
+                }
+            }
+        }
+        Console.WriteLine("{0}", maxSum);
+    }
+}
