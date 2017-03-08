c7c0d238cd4c7aeea281bea7938e76ad3bb6e23a
1337sh33p <ma6inoo@gmail.com>
2017-03-08 10:22:48 +0200

    Added tasks

---
+++ 02. CSharp Advanced/06. Exception Handling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class EnterNumbers
+{
+    static void Main()
+    {
+        //input
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        int[] check = new int[10];
+        //logic
+        try
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (numbers[i - 1] < numbers[i])
+                {
+                    check[i - 1] = numbers[i - 1];
+                    check[i] = numbers[i];
+                }
+                else if (numbers[i - 1] >= numbers[i])
+                {
+                    throw new Exception();
+                }
+            }
+            if (check[9] != 0 && check[9] <= 99)
+            {
+                Console.Write("1 <");
+                foreach (var num in check)
+                {
+                    Console.Write(" {0} <", num);
+                }
+                Console.WriteLine(" 100");
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+        }
+        catch (Exception)
+        {
+
+            Console.WriteLine("Exception");
+        }
+    }
+}
---
+++ 02. CSharp Advanced/06. Exception Handling/SquareRoot/SquareRoot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class SquareRoot
+{
+    static double Sqrt(double num)
+    {
+        if (num < 0)
+        {
+            throw new FormatException();
+        }
+        return Math.Sqrt(num);
+    }
+    static void Main()
+    {
+
+        try
+        {
+            double number = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F3}", Sqrt(number));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
