f2ac8b84ec02beab072fc2fae7aa9c96c725bdb2
1337sh33p <ma6inoo@gmail.com>
2017-03-09 19:38:11 +0200

    Added two of the exam tasks

---
+++ 02. CSharp Advanced/Exam/Cards/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Cards
+{
+    class Startup
+    {
+        public static void EvenApperanceCardPrinter(bool[]cardApperance)
+        {
+            string[] cards = { "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "Tc", "Jc", "Qc", "Kc", "Ac",
+            "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "Td", "Jd", "Qd", "Kd", "Ad",
+            "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "Th", "Jh", "Qh", "Kh", "Ah",
+            "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "Ts", "Js", "Qs", "Ks", "As"};
+
+            for (int i = 0; i < 52; i++)
+            {
+                if (cardApperance[i] == false)
+                {
+                    if (i != 51)
+                    {
+                        Console.Write("{0} ",cards[i]);
+                    }
+                    else
+                    {
+                        Console.Write(cards[i]);
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            int numberOfHands = int.Parse(Console.ReadLine());
+
+            bool[] evenApperance = new bool[52];
+            bool[] cardsAlreadyMet = new bool[52];
+
+            for (int hand = 1; hand <= numberOfHands; hand++)
+            {
+                ulong handInput = ulong.Parse(Console.ReadLine());
+
+                for (int i = 0; i < 52; i++)
+                {
+                    if (((int)(handInput >> i) & 1) == 1)
+                    {
+                        cardsAlreadyMet[i] = true;
+                        evenApperance[i] = !evenApperance[i];
+                    }
+                }
+            }
+
+            if (Array.IndexOf(cardsAlreadyMet, false) == -1)
+            {
+                Console.WriteLine("Full deck");
+            }
+            else
+            {
+                Console.WriteLine("Wa wa!");
+            }
+            EvenApperanceCardPrinter(evenApperance);
+        }
+    }
+}
---
+++ 02. CSharp Advanced/Exam/FunctionalNumeralSystem/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace FunctionalNumeralSystem
+{
+    class Program
+    {
+        static string ConvertToDecimal(string num)
+        {
+            string[] strangeSystem = { "ocaml", "haskell", "scala", "f#", "lisp", "rust", "ml", "clojure", "erlang", "standardml", "racket", "elm", "mercury", "commonlisp", "scheme", "curry" };
+            StringBuilder wtf = new StringBuilder();
+            wtf.Append(num);
+            wtf.Replace("haskell", "B");
+            wtf.Replace("f#", "D");
+            wtf.Replace("commonlisp", "N");
+            wtf.Replace("lisp", "E");
+            wtf.Replace("rust", "F");
+            wtf.Replace("clojure", "H");
+            wtf.Replace("racket", "K");
+            wtf.Replace("mercury", "M");
+            wtf.Replace("curry", "P");
+            wtf.Replace("ocaml", "A");
+            wtf.Replace("scala", "C");
+            wtf.Replace("erlang", "I");
+            wtf.Replace("standardml", "J");
+            wtf.Replace("elm", "L");
+            wtf.Replace("scheme", "O");
+            wtf.Replace("ml", "G");
+
+            var theNumber = wtf.ToString();
+
+            return theNumber;
+        }
+
+        static BigInteger BaseTransform(int NumBase, string number)
+        {
+            BigInteger result = 0;
+            foreach (var digit in number)
+            {
+                result = result * NumBase + (digit - 'A');
+            }
+            return result;
+        }
+
+        static void Main()
+        {
+            //input
+            string[] separator = { ", " };
+            var theNumbers = Console.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            // logic
+           var number1 = ConvertToDecimal(theNumbers[0]);
+           var number2 = ConvertToDecimal(theNumbers[1]);
+           var number3 = ConvertToDecimal(theNumbers[2]);
+
+            var num1InDeci = BaseTransform(16, number1);
+            var num2InDeci = BaseTransform(16, number2);
+            var num3InDeci = BaseTransform(16, number3);
+
+            //output = product of three numbers
+            BigInteger finalResult = num1InDeci * num2InDeci * num3InDeci;
+            Console.WriteLine(finalResult);
+        }
+    }
+}
